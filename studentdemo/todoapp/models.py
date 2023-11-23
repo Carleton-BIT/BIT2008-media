f3b906b4f388bd79f9f87233e25910727482d771
James Brunet <james@brunet.dev>
2023-11-23 12:51:58 -0500

    Resize and compress images

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 from django.conf import settings
-
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
     image = models.ImageField(upload_to='task_images/', default='default.jpg')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        MAX_SIZE = (500, 500)
+
+        # Open the uploaded image
+        img = Image.open(self.image.path)
+
+        if img.height > MAX_SIZE[1] or img.width > MAX_SIZE[0]:
+
+            # Resize the image using LANCZOS filter
+            img.thumbnail(MAX_SIZE, Image.Resampling.LANCZOS)
+
+            # Save the resized image back to self.image
+            img_file = BytesIO()
+            img.save(img_file, 'JPEG', quality=90)  # Adjust quality as needed
+            file_name = self.image.name
+            self.image.save(file_name, ContentFile(img_file.getvalue()), save=False)
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.description
